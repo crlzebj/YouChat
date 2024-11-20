c12987129a6e879709dfa19e71ebb28202bdc1d9
crlzebj <2393929142@qq.com>
2024-11-20 10:52:10 +0800

    实现群头像上传功能

---
+++ src/main/java/com/zjx/youchat/configuration/ProjectConfig.java
@@ -0,0 +1,15 @@
+package com.zjx.youchat.configuration;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+public class ProjectConfig {
+    @Value("${you-chat.server.data-path}")
+    private String serverDataPath;
+
+    @Value("${you-chat.client.data-path}")
+    private String clientDataPath;
+}
--- src/main/java/com/zjx/youchat/service/impl/ChatGroupServiceImpl.java
+++ src/main/java/com/zjx/youchat/service/impl/ChatGroupServiceImpl.java
@@ -1,14 +1,18 @@
 package com.zjx.youchat.service.impl;
 
-import com.zjx.youchat.constant.UserConstant;
+import com.zjx.youchat.configuration.ProjectConfig;
+import com.zjx.youchat.exception.BusinessException;
 import com.zjx.youchat.mapper.ChatGroupMapper;
+import com.zjx.youchat.mapper.UserContactMapper;
 import com.zjx.youchat.pojo.dto.ChatGroupRegisterDTO;
 import com.zjx.youchat.pojo.po.ChatGroup;
+import com.zjx.youchat.pojo.po.UserContact;
 import com.zjx.youchat.service.ChatGroupService;
-import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.io.File;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -16,6 +20,12 @@ import java.util.List;
 public class ChatGroupServiceImpl implements ChatGroupService {
 	@Autowired
 	private ChatGroupMapper chatGroupMapper;
+
+	@Autowired
+	private UserContactMapper userContactMapper;
+
+	@Autowired
+	private ProjectConfig projectConfig;
 
 	@Override
 	public void insertChatGroup(ChatGroup chatGroup) {
@@ -43,6 +53,7 @@ public class ChatGroupServiceImpl implements ChatGroupService {
 	}
 
 	@Override
+	@Transactional
 	public void register(String ownerId, ChatGroupRegisterDTO chatGroupRegisterDTO) {
 		ChatGroup chatGroup = new ChatGroup();
 		chatGroup.setId(chatGroupRegisterDTO.getId());
@@ -52,6 +63,23 @@ public class ChatGroupServiceImpl implements ChatGroupService {
 		chatGroup.setOwnerId(ownerId);
 		chatGroup.setGroupNotice(chatGroupRegisterDTO.getGroupNotice());
 		chatGroup.setCreateTime(LocalDateTime.now());
+		try {
+			File file1 = new File(projectConfig.getServerDataPath(), "1.png");
+			chatGroupRegisterDTO.getAvatarFile().transferTo(file1.getAbsoluteFile());
+			File file2 = new File(projectConfig.getServerDataPath(), "2.png");
+			chatGroupRegisterDTO.getAvatarThumbFile().transferTo(file2.getAbsoluteFile());
+		} catch (Exception e) {
+			throw new BusinessException("头像上传失败，请稍后重试");
+		}
 		chatGroupMapper.insertChatGroup(chatGroup);
+
+		UserContact userContact = new UserContact();
+		userContact.setUserId(ownerId);
+		userContact.setContactId(chatGroup.getId());
+		userContact.setContactType(1);
+		userContact.setContactPermission(1);
+		userContact.setCreateTime(LocalDateTime.now());
+		userContact.setLastUpdateTime(LocalDateTime.now());
+		userContactMapper.insertUserContact(userContact);
 	}
 }
